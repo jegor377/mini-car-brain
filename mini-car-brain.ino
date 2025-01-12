83a47d571f6ded4766f0790d0cdd5b5610280327
Igor Santarek <jegor377@gmail.com>
2025-01-13 00:12:04 +0100

    Działający robot

--- mini-car-brain.ino
+++ mini-car-brain.ino
@@ -6,10 +6,10 @@
 #define ACC_SCL 16
 #define ACC_SDA 4
 
-#define LED1_PIN 2
-#define LED2_PIN 5
-#define LED3_PIN 15
-#define LED4_PIN 13
+#define FRONT_LEFT_LED_PIN 2
+#define FRONT_RIGHT_LED_PIN 13
+#define BACK_LEFT_LED_PIN 5
+#define BACK_RIGHT_LED_PIN 15
 
 #define STBY1_PIN 17
 
@@ -34,6 +34,7 @@
 #define LED_PWM_RESOLUTION 10
 #define LED_PWM_FREQ 100
 #define MAX_LED_DC 1 << LED_PWM_RESOLUTION
+#define HALF_MAX_LED_DC MAX_LED_DC >> 1
 #define MAX_MOTOR_DC 255
 
 #define CMD_SIZE 14
@@ -55,13 +56,13 @@
 #define WHEEL_RAD WHEEL_DIM / 2
 #define WHEEL_CIRCUMFERENCE WHEEL_RAD * 2 * PI
 #define MOTOR_RPS MOTOR_RPM / 60
-#define MOTOR_MAX_SPEED WHEEL_CIRCUMFERENCE * MOTOR_RPS
+#define MOTOR_MAX_SPEED WHEEL_CIRCUMFERENCE * MOTOR_RPS // cm/s
 
 #define MAX_SPEED 1000.0
 
-#define WHEEL_SPAN 76 // mm
+#define WHEEL_SPAN 76 / 10 // cm
 
-#define ROTATION_FACTOR 1
+#define ROTATION_FACTOR 2
 
 // Check if Bluetooth is available
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
@@ -78,36 +79,31 @@ String device_name = "CONAN II";
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified();
 BluetoothSerial SerialBT;
 
-char cmd[CMD_SIZE], program[PROGRAM_CMDS_COUNT][CMD_SIZE];
+char cmd[CMD_SIZE], program[PROGRAM_CMDS_COUNT][CMD_SIZE], next_cmd[CMD_SIZE];
 int mode = FREE_CONTROL, instruction = IDLE_INSTRUCTION;
 
-int left_speed, right_speed;
-int desired_rotation, desired_distance, desired_speed = 100;
+int left_speed = 0, right_speed = 0;
+int desired_rotation = 0, desired_distance = 0, desired_speed = 100;
 float forward_displacement, rotation_displacement;
 int instruction_iter = 0, set_instructions = 0;
 unsigned long last_time;
 
-bool is_going_forward, is_going_backward;
-bool is_rotating_left, is_rotating_right;
-
-void clear_program() {
-  for(int i = 0; i < PROGRAM_CMDS_COUNT; i++) {
-    strcpy(program[i], ";");
-  }
-}
+bool is_going_forward = false, is_going_backward = false;
+bool is_rotating_left = false, is_rotating_right = false;
+bool was_bt_connected = false;
 
 void set_left_motors() {
   int duty_cycle = map(abs(left_speed), 0, MAX_SPEED, 0, MAX_MOTOR_DC);
 
   if(left_speed > 0) { // forward
-    digitalWrite(CIN1_PIN, HIGH);
+    digitalWrite(CIN1_PIN, LOW);
     digitalWrite(DIN1_PIN, HIGH);
-    digitalWrite(CIN2_PIN, LOW);
+    digitalWrite(CIN2_PIN, HIGH);
     digitalWrite(DIN2_PIN, LOW);
   } else if(left_speed < 0) { // backward
-    digitalWrite(CIN1_PIN, LOW);
+    digitalWrite(CIN1_PIN, HIGH);
     digitalWrite(DIN1_PIN, LOW);
-    digitalWrite(CIN2_PIN, HIGH);
+    digitalWrite(CIN2_PIN, LOW);
     digitalWrite(DIN2_PIN, HIGH);
   } else { // stop
     digitalWrite(CIN1_PIN, LOW);
@@ -123,16 +119,16 @@ void set_left_motors() {
 void set_right_motors() {
   int duty_cycle = map(abs(right_speed), 0, MAX_SPEED, 0, MAX_MOTOR_DC);
 
-  if(left_speed > 0) { // forward
+  if(right_speed > 0) { // forward
     digitalWrite(AIN1_PIN, HIGH);
-    digitalWrite(BIN1_PIN, HIGH);
-    digitalWrite(AIN2_PIN, LOW);
-    digitalWrite(BIN2_PIN, LOW);
-  } else if(left_speed < 0) { // backward
-    digitalWrite(AIN1_PIN, LOW);
     digitalWrite(BIN1_PIN, LOW);
-    digitalWrite(AIN2_PIN, HIGH);
+    digitalWrite(AIN2_PIN, LOW);
     digitalWrite(BIN2_PIN, HIGH);
+  } else if(right_speed < 0) { // backward
+    digitalWrite(AIN1_PIN, LOW);
+    digitalWrite(BIN1_PIN, HIGH);
+    digitalWrite(AIN2_PIN, HIGH);
+    digitalWrite(BIN2_PIN, LOW);
   } else { // stop
     digitalWrite(AIN1_PIN, LOW);
     digitalWrite(BIN1_PIN, LOW);
@@ -144,41 +140,55 @@ void set_right_motors() {
   analogWrite(PWMB_PIN, duty_cycle);
 }
 
-void clear_cmd() {
-  strcpy(cmd, ";");
-}
-
-void next_instruction() {
-  if(instruction_iter < PROGRAM_CMDS_COUNT) {
-    strcpy(cmd, program[instruction_iter++]);
-  } else {
-    mode = FREE_CONTROL;
-    instruction = IDLE_INSTRUCTION;
+void clear_cmd(char* c) {
+  c[0] = CMD_TERMINATOR;
+  for(int i = 1; i < CMD_SIZE; i++) {
+    c[i] = 0;
   }
 }
 
+void clear_program() {
+  for(int i = 0; i < PROGRAM_CMDS_COUNT; i++) {
+    clear_cmd(program[i]);
+  }
+}
+
+void next_instruction() {
+  if(mode != PROGRAM_CONTROL) return;
+  if(instruction_iter + 1 < PROGRAM_CMDS_COUNT && program[instruction_iter + 1][0] != CMD_TERMINATOR) {
+    clear_cmd(next_cmd);
+    strcpy(next_cmd, program[++instruction_iter]);
+  } else {
+    mode = FREE_CONTROL;
+  }
+  instruction = IDLE_INSTRUCTION;
+}
+
+// in cm per sec
 inline float motor_speed(int speed) {
   return MOTOR_MAX_SPEED * (float)speed / MAX_SPEED;
 }
 
+inline float rad2deg(float rad) {
+  return rad * 180.0 / PI;
+}
+
 void setup() {
-  ledcAttach(LED1_PIN, LED_PWM_FREQ, LED_PWM_RESOLUTION);
-  ledcAttach(LED2_PIN, LED_PWM_FREQ, LED_PWM_RESOLUTION);
-  ledcAttach(LED3_PIN, LED_PWM_FREQ, LED_PWM_RESOLUTION);
-  ledcAttach(LED4_PIN, LED_PWM_FREQ, LED_PWM_RESOLUTION);
-  ledcOutputInvert(LED2_PIN, true);
-  ledcOutputInvert(LED3_PIN, true);
+  ledcAttach(FRONT_LEFT_LED_PIN, LED_PWM_FREQ, LED_PWM_RESOLUTION);
+  ledcAttach(BACK_LEFT_LED_PIN, LED_PWM_FREQ, LED_PWM_RESOLUTION);
+  ledcAttach(BACK_RIGHT_LED_PIN, LED_PWM_FREQ, LED_PWM_RESOLUTION);
+  ledcAttach(FRONT_RIGHT_LED_PIN, LED_PWM_FREQ, LED_PWM_RESOLUTION);
 
   // inform setup started
-  ledcWrite(LED1_PIN, MAX_LED_DC);
-  ledcWrite(LED2_PIN, MAX_LED_DC);
-  ledcWrite(LED3_PIN, MAX_LED_DC);
-  ledcWrite(LED4_PIN, MAX_LED_DC);
+  ledcWrite(FRONT_LEFT_LED_PIN, HALF_MAX_LED_DC);
+  ledcWrite(BACK_LEFT_LED_PIN, HALF_MAX_LED_DC);
+  ledcWrite(BACK_RIGHT_LED_PIN, HALF_MAX_LED_DC);
+  ledcWrite(FRONT_RIGHT_LED_PIN, HALF_MAX_LED_DC);
   delay(200);
-  ledcWrite(LED1_PIN, 0);
-  ledcWrite(LED2_PIN, 0);
-  ledcWrite(LED3_PIN, 0);
-  ledcWrite(LED4_PIN, 0);
+  ledcWrite(FRONT_LEFT_LED_PIN, 0);
+  ledcWrite(BACK_LEFT_LED_PIN, 0);
+  ledcWrite(BACK_RIGHT_LED_PIN, 0);
+  ledcWrite(FRONT_RIGHT_LED_PIN, 0);
 
   pinMode(STBY1_PIN, OUTPUT);
   pinMode(STBY2_PIN, OUTPUT);
@@ -204,8 +214,6 @@ void setup() {
 
   Serial.begin(115200);
   SerialBT.begin(device_name);  //Bluetooth device name
-  //SerialBT.deleteAllBondedDevices(); // Uncomment this to delete paired devices; Must be called after begin
-  // Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
 
   if(!accel.begin(ADXL345_DEFAULT_ADDRESS, ACC_SDA, ACC_SCL))
   {
@@ -218,20 +226,25 @@ void setup() {
 
   // inform setup is done  
   delay(200);
-  ledcWrite(LED1_PIN, MAX_LED_DC);
-  ledcWrite(LED2_PIN, MAX_LED_DC);
-  ledcWrite(LED3_PIN, MAX_LED_DC);
-  ledcWrite(LED4_PIN, MAX_LED_DC);
+  ledcWrite(FRONT_LEFT_LED_PIN, HALF_MAX_LED_DC);
+  ledcWrite(BACK_LEFT_LED_PIN, HALF_MAX_LED_DC);
+  ledcWrite(BACK_RIGHT_LED_PIN, HALF_MAX_LED_DC);
+  ledcWrite(FRONT_RIGHT_LED_PIN, HALF_MAX_LED_DC);
   delay(1000);
-  ledcWrite(LED1_PIN, 0);
-  ledcWrite(LED2_PIN, 0);
-  ledcWrite(LED3_PIN, 0);
-  ledcWrite(LED4_PIN, 0);
+  ledcWrite(FRONT_LEFT_LED_PIN, 0);
+  ledcWrite(BACK_LEFT_LED_PIN, 0);
+  ledcWrite(BACK_RIGHT_LED_PIN, 0);
+  ledcWrite(FRONT_RIGHT_LED_PIN, 0);
+
+  clear_cmd(cmd);
+
+  Serial.println("Configuration complete! :)");
 }
 
 void loop() {
+  unsigned long now = micros();
   if(mode == PROGRAM_CONTROL) {
-    float dt = (float)(millis() - last_time) / 1000.0;
+    float dt = (float)(now - last_time) / 1000000.0;
     switch(instruction) {
       case ROTATE_INSTRUCTION: {
         if(desired_rotation == 0) {
@@ -240,9 +253,9 @@ void loop() {
         }
 
         if(is_rotating_left || is_rotating_right) {
-          rotation_displacement += ROTATION_FACTOR * 2 * motor_speed(desired_speed) * dt / WHEEL_SPAN;
+          rotation_displacement += rad2deg(ROTATION_FACTOR * 2 * motor_speed(desired_speed) * dt / WHEEL_SPAN);
 
-          if(rotation_displacement >= desired_rotation) {
+          if(rotation_displacement >= (float)abs(desired_rotation)) {
             left_speed = 0;
             right_speed = 0;
             set_left_motors();
@@ -274,7 +287,7 @@ void loop() {
         if(is_going_forward || is_going_backward) {
           forward_displacement += motor_speed(desired_speed) * dt;
 
-          if(forward_displacement >= desired_distance) {
+          if(forward_displacement >= (float)abs(desired_distance)) {
             left_speed = 0;
             right_speed = 0;
             set_left_motors();
@@ -300,90 +313,154 @@ void loop() {
     }
   }
 
-  if (SerialBT.available()) {
-    SerialBT.readBytesUntil(CMD_TERMINATOR, cmd, CMD_SIZE);
-    if(mode == PROGRAM_CONTROL) {
-      instruction_iter--;
+  if(!was_bt_connected && SerialBT.hasClient()) {
+    for(int i = 0; i < HALF_MAX_LED_DC; i++) {
+      ledcWrite(FRONT_LEFT_LED_PIN, i);
+      ledcWrite(FRONT_RIGHT_LED_PIN, i);
+      ledcWrite(BACK_LEFT_LED_PIN, i);
+      ledcWrite(BACK_RIGHT_LED_PIN, i);
+      delay(1);
     }
+
+    for(int i = HALF_MAX_LED_DC; i >= 0; i--) {
+      ledcWrite(FRONT_LEFT_LED_PIN, i);
+      ledcWrite(FRONT_RIGHT_LED_PIN, i);
+      ledcWrite(BACK_LEFT_LED_PIN, i);
+      ledcWrite(BACK_RIGHT_LED_PIN, i);
+      delay(1);
+    }
+    ledcWrite(FRONT_LEFT_LED_PIN, 0);
+    ledcWrite(FRONT_RIGHT_LED_PIN, 0);
+    ledcWrite(BACK_LEFT_LED_PIN, 0);
+    ledcWrite(BACK_RIGHT_LED_PIN, 0);
+    SerialBT.println("ready");
+    was_bt_connected = true;
+  } else if(was_bt_connected && !SerialBT.hasClient()) {
+    ledcWrite(FRONT_LEFT_LED_PIN, 0);
+    ledcWrite(FRONT_RIGHT_LED_PIN, 0);
+    ledcWrite(BACK_LEFT_LED_PIN, 0);
+    ledcWrite(BACK_RIGHT_LED_PIN, 0);
+    delay(100);
+    ledcWrite(FRONT_LEFT_LED_PIN, HALF_MAX_LED_DC);
+    delay(100);
+    ledcWrite(FRONT_LEFT_LED_PIN, 0);
+    ledcWrite(FRONT_RIGHT_LED_PIN, HALF_MAX_LED_DC);
+    delay(100);
+    ledcWrite(FRONT_RIGHT_LED_PIN, 0);
+    ledcWrite(BACK_LEFT_LED_PIN, HALF_MAX_LED_DC);
+    delay(100);
+    ledcWrite(BACK_LEFT_LED_PIN, 0);
+    ledcWrite(BACK_RIGHT_LED_PIN, HALF_MAX_LED_DC);
+    delay(100);
+    ledcWrite(BACK_RIGHT_LED_PIN, 0);
+    was_bt_connected = false;
+  }
+
+  if(mode == PROGRAM_CONTROL && instruction == IDLE_INSTRUCTION) {
+    strcpy(cmd, next_cmd);
+  }
+
+  if (SerialBT.available()) {
+    clear_cmd(cmd);
+    SerialBT.readBytesUntil(CMD_TERMINATOR, cmd, CMD_SIZE);
   }
 
   if(mode == PROGRAM_DEFINITION && cmd[0] > 'C' ) {
     if(instruction_iter + 1 < 100) {
       strcpy(program[instruction_iter++], cmd);
     }
-  }
-
-  int k;
-  switch(cmd[0]) {
-    case 'A': {
-      k = sscanf(cmd, "A%d,%d;", &left_speed, &right_speed);
-      if(k == 2) {
-        left_speed = constrain(left_speed, -MAX_SPEED, MAX_SPEED);
-        right_speed = constrain(right_speed, -MAX_SPEED, MAX_SPEED);
-        set_left_motors();
-        set_right_motors();
-        mode = FREE_CONTROL;
-        clear_cmd();
-      }
-    } break;
-    case 'B': {
-      clear_program();
-      mode = PROGRAM_DEFINITION;
-      instruction = IDLE_INSTRUCTION;
-      instruction_iter = 0;
-      clear_cmd();
-    } break;
-    case 'C': {
-      mode = PROGRAM_CONTROL;
-      clear_cmd();
-    } break;
-    case 'D': { // rotate
-      k = sscanf(cmd, "D%d;", &desired_rotation);
-      if(k == 1) {
-        rotation_displacement = 0;
-        is_rotating_left = false;
-        is_rotating_right = false;
-        instruction = ROTATE_INSTRUCTION;
-      }
-    } break;
-    case 'E': {
-      k = sscanf(cmd, "E%d;", &desired_distance);
-      if(k == 1) {
-        forward_displacement = 0;
-        is_going_forward = false;
-        is_going_backward = false;
-        instruction = MOVE_INSTRUCTION;
-      }
-    } break;
-    case 'F': {
-      k = sscanf(cmd, "F%d;", &desired_speed);
-      if(k == 1) {
-        desired_speed = abs(constrain(desired_speed, -MAX_SPEED, MAX_SPEED));
+  } else {
+    int k;
+    switch(cmd[0]) {
+      case 'A': {
+        k = sscanf(cmd, "A%d,%d", &left_speed, &right_speed);
+        if(k == 2) {
+          left_speed = constrain(left_speed, -MAX_SPEED, MAX_SPEED);
+          right_speed = constrain(right_speed, -MAX_SPEED, MAX_SPEED);
+          set_left_motors();
+          set_right_motors();
+          mode = FREE_CONTROL;
+        }
+      } break;
+      case 'B': {
+        clear_program();
+        mode = PROGRAM_DEFINITION;
+        instruction = IDLE_INSTRUCTION;
+        instruction_iter = 0;
+        Serial.println("Przejscie do trybu programowania");
+      } break;
+      case 'C': {
+        mode = PROGRAM_CONTROL;
+        instruction_iter = 0;
+        Serial.println("Przejscie do trybu wykonywania programu");
+        strcpy(next_cmd, program[0]);
+        Serial.print("Ustawienie nastepnej komendy ");
+        Serial.println(next_cmd);
+      } break;
+      case 'D': { // rotate
+        k = sscanf(cmd, "D%d", &desired_rotation);
+        if(k == 1) {
+          rotation_displacement = 0;
+          is_rotating_left = false;
+          is_rotating_right = false;
+          instruction = ROTATE_INSTRUCTION;
+        }
+      } break;
+      case 'E': {
+        k = sscanf(cmd, "E%d", &desired_distance);
+        if(k == 1) {
+          forward_displacement = 0;
+          is_going_forward = false;
+          is_going_backward = false;
+          instruction = MOVE_INSTRUCTION;
+        }
+      } break;
+      case 'F': {
+        k = sscanf(cmd, "F%d", &desired_speed);
+        if(k == 1) {
+          desired_speed = abs(constrain(desired_speed, -MAX_SPEED, MAX_SPEED));
+          next_instruction();
+        }
+      } break;
+      case 'G': {
+        instruction_iter = -1;
         next_instruction();
+        break;
       }
-    } break;
-    case 'G': {
-      instruction_iter = 0;
-      break;
+      case 'H': {
+        ledcWrite(FRONT_LEFT_LED_PIN, MAX_LED_DC);
+        ledcWrite(FRONT_RIGHT_LED_PIN, MAX_LED_DC);
+        Serial.println("Front lights ON");
+        next_instruction();
+      } break;
+      case 'I': {
+        ledcWrite(FRONT_LEFT_LED_PIN, 0);
+        ledcWrite(FRONT_RIGHT_LED_PIN, 0);
+        Serial.println("Front lights OFF");
+        next_instruction();
+      } break;
+      case 'J': {
+        digitalWrite(STBY1_PIN, HIGH);
+        digitalWrite(STBY2_PIN, HIGH);
+        next_instruction();
+      } break;
+      case 'K': {
+        digitalWrite(STBY1_PIN, LOW);
+        digitalWrite(STBY2_PIN, LOW);
+        next_instruction();
+      } break;
+      case 'L': {
+        for(int i = 0; i < PROGRAM_CMDS_COUNT; i++) {
+          if(program[i][0] != ';') {
+            SerialBT.println(program[i]);
+          }
+        }
+        next_instruction();
+      } break;
+      default: break;
     }
-    case 'H': {
-      ledcWrite(LED1_PIN, MAX_LED_DC);
-      ledcWrite(LED4_PIN, MAX_LED_DC);
-    } break;
-    case 'I': {
-      ledcWrite(LED1_PIN, 0);
-      ledcWrite(LED4_PIN, 0);
-    } break;
-    case 'J': {
-      digitalWrite(STBY1_PIN, HIGH);
-      digitalWrite(STBY2_PIN, HIGH);
-    } break;
-    case 'K': {
-      digitalWrite(STBY1_PIN, LOW);
-      digitalWrite(STBY2_PIN, LOW);
-    } break;
-    default: break;
   }
 
-  last_time = millis();
+  clear_cmd(cmd);
+  last_time = now;
 }
